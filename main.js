d00955ce2dd6dca314f2f2ab6d9d5040ece905f9
Nekros <abdelo_y@etna-alternance.net>
2021-11-02 00:15:11 +0100

    ajout de la page d'accueille

---
+++ main.js
@@ -0,0 +1,13 @@
+var monstre = [
+    { id = 1, name: Cat_Sidhe, PV: 50, ATT: 25, DEFF: 10, Mana: 0 },
+    { id = 2, name: monstre2, PV: 50, ATT: 30, DEFF: 15, Mana: 10 },
+    { id = 3, name: Banshee, PV: 90, ATT: 50, DEFF: 20, Mana: 15 },
+    { id = 4, name: Addanc, PV: 90, ATT: 50, DEFF: 25, Mana: 20 }
+];
+
+var perso = [
+    { id = 1, name: Cat_Sidhe, PV: 50, ATT: 25, DEFF: 10, Mana: 0 },
+    { id = 2, name: monstre2, PV: 50, ATT: 30, DEFF: 15, Mana: 10 },
+    { id = 3, name: Banshee, PV: 90, ATT: 50, DEFF: 20, Mana: 15 },
+    { id = 4, name: Addanc, PV: 90, ATT: 50, DEFF: 25, Mana: 20 }
+];
